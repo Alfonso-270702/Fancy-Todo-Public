0a0feee0622ee5e7180f87668ab06a877bd593f0
Alfonso-270702 <tebaksiapa27@gmail.com>
2020-07-13 20:13:38 +0700

    add route get to test heroku server

--- server/routers/home.js
+++ server/routers/home.js
@@ -1,6 +1,10 @@
 const route = require('express').Router()
 const HomeController = require('../controllers/homeController')
 
+
+route.get('/',function(req,res){
+    res.status(200).json({ msg: 'Welcome Home'})
+})
 route.post('/register',HomeController.register)
 
 route.post('/login',HomeController.login)
