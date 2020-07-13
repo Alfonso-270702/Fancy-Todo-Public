207b6de3b930b5b709a9d3f79d609dcb74877471
Alfonso-270702 <tebaksiapa27@gmail.com>
2020-07-13 20:00:48 +0700

    add Procfile, add config development

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const route = require('./routers/index')
 const errHandler = require('./middleware/errorHandler')
 const cors = require('cors')
